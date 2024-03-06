d1a5e7a9d4d9242620695b2f31d27028b7de516b
Elvis Orrico | Analista de Infraestrutura <elvis.orrico@nucleoengenharia.com.br>
2024-03-06 19:32:38 -0300

    Alterar rota

--- src/server.js
+++ src/server.js
@@ -18,10 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-<<<<<<< HEAD
-  console.log("Boas-vindas à API do AllBooks")
-  console.log("API disponível através da url http://localhost:8000")
-=======
   console.log("Acesse essa API em http://localhost:8000")
->>>>>>> b515b531b2824805b570267d0227c6eed6ebe00a
 })
